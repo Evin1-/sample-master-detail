bdd888857d596715d3448520c8c58eb01973eeb6
Evin1- <edwin.hdz.12.0@gmail.com>
2016-03-13 23:44:39 -0400

    Add multiple TODOs

--- app/src/main/java/com/example/test/samplemasterdetail/App.java
+++ app/src/main/java/com/example/test/samplemasterdetail/App.java
@@ -15,6 +15,11 @@ public class App extends Application {
     // TODO: 3/10/16 Remove unused strings values
     // TODO: 3/10/16 Add scrolling behavior
     // TODO: 3/13/16 Check if details is empty on tablet layout
+    // TODO: 3/13/16 Make DetailsActivity pretty
+    // TODO: 3/13/16 Create note docs and commenting 
+    // TODO: 3/13/16 Implement local search functionality 
+    // TODO: 3/13/16 Favorite characters
+    // TODO: 3/13/16 Add animations
 
     @Override
     public void onCreate() {
